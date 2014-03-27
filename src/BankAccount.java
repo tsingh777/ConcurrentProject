96b05b7d6f66b8563e6eea1b761b478faee80346
tsingh7 <tsingh7@10.136.79.251>
2014-03-27 10:55:15 -0400

    Added balance field to BankAccount and methods to manipulate it Made Simulator class that will run the simulation Gave User a BankAccount Field and default constructor.

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,50 @@
 
 public class BankAccount {
+	private double balance;
 
+	public BankAccount(){
+		this.balance = 4000.00;//start with 4000?
+	}
+	public BankAccount(double avalibleAmount) {
+		this.balance = avalibleAmount;
+	}
+
+	
+	/**
+	 * add to the amount avalible
+	 * @param amount
+	 */
+	public void deposit(double amount){
+		this.balance+= amount;
+	}
+	
+	/**
+	 * remove from amount avalible 
+	 * @param amount
+	 * @return avalibleAmount after withdraw
+	 */
+	public double withdraw(double amount){
+		if(amount > this.balance){
+			return 0;
+		}
+		else{
+			this.balance -=amount;
+			return (this.balance);
+		}
+	}
+	/**
+	 * @return the balace
+	 */
+	public double getBalance() {
+		return balance;
+	}
+
+	/**
+	 * @param avalibleAmount the avalibleAmount to set
+	 */
+	public void setbalance(double avalibleAmount) {
+		this.balance = avalibleAmount;
+	}
+	
+	
 }
--- src/User.java
+++ src/User.java
@@ -1,8 +1,13 @@
+import java.io.File;
+
 
 public class User {
-
+	BankAccount bank;
+	//File transaction; //maybe have file with a set of transaction that can be read?
+	
 	public User() {
-		// TODO Auto-generated constructor stub
+		this.bank = new BankAccount();
 	}
+	
 
 }
