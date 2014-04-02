29c7e6a2b45bcd1ffdb4763fa27436dafce42bd4
tsingh7 <tsingh7@10.136.109.228>
2014-04-02 15:40:36 -0400

    Eh

--- src/Simulator.java
+++ src/Simulator.java
@@ -6,7 +6,7 @@ public class Simulator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/*try {
+		try {
 			PrintWriter pw = new PrintWriter("accounts.txt");
 			for(int i=0; i<10; i++){
 				pw.println(i +  " " + (int)(Math.random()*10000)+ ".00" );
@@ -17,7 +17,7 @@ public class Simulator {
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}*/
+		}
 
 	}
 
