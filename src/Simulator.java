b491e991d4f61593ecd67cc9168824589e246509
tsingh7 <tsingh7@10.136.109.228>
2014-04-02 15:35:46 -0400

    accountfile created

--- src/Simulator.java
+++ src/Simulator.java
@@ -1,9 +1,23 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 
 public class Simulator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		/*try {
+			PrintWriter pw = new PrintWriter("accounts.txt");
+			for(int i=0; i<10; i++){
+				pw.println(i +  " " + (int)(Math.random()*10000)+ ".00" );
+			}
+			pw.flush();
+			pw.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
 
 	}
 
