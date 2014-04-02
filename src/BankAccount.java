b9f71aba7a8989bd17de2399ead33edc4073c624
Wilson Yeung <wyeung@ufl.edu>
2014-04-02 14:56:50 -0400

    updates

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,37 +1,44 @@
-
 public class BankAccount {
 	private double balance;
+	private int accountNumber;
 
-	public BankAccount(){
-		this.balance = 4000.00;//start with 4000?
+	public BankAccount() {
+		this.balance = 4000.00;// start with 4000?
 	}
+
 	public BankAccount(double avalibleAmount) {
 		this.balance = avalibleAmount;
 	}
-
 	
+	public BankAccount(double avalibleAmount, int accountNumber) {
+		this.balance = avalibleAmount;
+		this.accountNumber = accountNumber;
+	}
+
 	/**
 	 * add to the amount avalible
+	 * 
 	 * @param amount
 	 */
-	public void deposit(double amount){
-		this.balance+= amount;
+	public void deposit(double amount) {
+		this.balance += amount;
 	}
-	
+
 	/**
-	 * remove from amount avalible 
+	 * remove from amount avalible
+	 * 
 	 * @param amount
 	 * @return avalibleAmount after withdraw
 	 */
-	public double withdraw(double amount){
-		if(amount > this.balance){
+	public double withdraw(double amount) {
+		if (amount > this.balance) {
 			return 0;
-		}
-		else{
-			this.balance -=amount;
+		} else {
+			this.balance -= amount;
 			return (this.balance);
 		}
 	}
+
 	/**
 	 * @return the balace
 	 */
@@ -40,11 +47,25 @@ public class BankAccount {
 	}
 
 	/**
-	 * @param avalibleAmount the avalibleAmount to set
+	 * @param avalibleAmount
+	 *            the avalibleAmount to set
 	 */
 	public void setbalance(double avalibleAmount) {
 		this.balance = avalibleAmount;
 	}
-	
-	
+
+	/**
+	 * @return the accountNumber
+	 */
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+
+	/**
+	 * @param accountNumber the accountNumber to set
+	 */
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
 }
--- src/User.java
+++ src/User.java
@@ -1,13 +1,13 @@
 import java.io.File;
 
-
 public class User {
 	BankAccount bank;
-	//File transaction; //maybe have file with a set of transaction that can be read?
-	
+
+	// File transaction; //maybe have file with a set of transaction that can be
+	// read?
+
 	public User() {
 		this.bank = new BankAccount();
 	}
-	
 
 }
