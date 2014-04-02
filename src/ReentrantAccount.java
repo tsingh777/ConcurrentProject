489095f3e340c15763a2bdc5d161b6426d811045
Jin <jinl55@yahoo.com>
2014-04-02 15:46:09 -0400

    Filled out Reentrant a bit

--- src/ReentrantAccount.java
+++ src/ReentrantAccount.java
@@ -1,4 +1,37 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 
 public class ReentrantAccount extends BankAccount {
-
+	private double balance;
+	private int accountNumber;
+	private Lock lock;
+	
+	public ReentrantAccount() {
+		this.balance = 4000.00;
+		this.balance = 0;
+		lock = new ReentrantLock();
+	}
+	
+	public ReentrantAccount(double balance, int accountNumber) {
+		this.balance = balance;
+		this.accountNumber = accountNumber;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+	public double withdraw(double amount) {
+		if(balance < 0) {
+			return -1;
+		}
+		else if((balance-amount < 0)) {
+			return -1;
+		}
+		else {
+			balance -= amount;
+			return amount;
+		}
+	}
 }
